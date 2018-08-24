df7bd5fc391d46b3ab826a8a3d7e3ccb4cc3db55
Vincentius Gerardo <32858920+VincentiusGerardo@users.noreply.github.com>
2018-08-24 14:11:46 +0700

    Update and rename mUtama.php to Model_utama.php

--- application/models/Model_utama.php
+++ application/models/Model_utama.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class mUtama extends CI_Model{
+    class Model_utama extends CI_Model{
 
         public function __construct(){
             parent::__construct();
