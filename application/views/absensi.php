d8334ab7df4e7431f6654ba1d27b26b4af5a2aac
Vincentius Gerardo <32858920+VincentiusGerardo@users.noreply.github.com>
2018-08-28 10:25:55 +0700

    Update absensi.php

--- application/views/absensi.php
+++ application/views/absensi.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <title>MaXXi | Login Page</title>
+        <title>MaXXi | Absensi</title>
 
         <!--jQuery 3-->
         <script src="<?php echo base_url('js/jquery/jquery.min.js'); ?>"></script>
