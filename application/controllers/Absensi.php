068e070ac9d55faa58a883a0e1c9d2c4cd87a4a2
Vincentius Gerardo <vincentiusgerardo11@gmail.com>
2018-10-13 15:30:04 +0700

    udah ga perlu load model

--- application/controllers/Absensi.php
+++ application/controllers/Absensi.php
@@ -5,31 +5,30 @@
 
         public function __construct(){
             parent::__construct();
-            $this->load->model('model_utama');
         }
 
         public function index(){
             $this->load->view('absensi');
         }
-        
+
         public function doAbsensi(){
             $this->form_validation->set_rules('x','KodeKaryawan','required|xss_clean|trim');
             if($this->form_validation->run() == TRUE){
                 $kode = $this->input->post('x');
                 $tgl = date('Y-m-d');
-                
+
                 $result = $this->model_utama->absensi($kode,$tgl);
             }else{
                 echo "Kode Karyawan Kosong!";
             }
         }
-        
+
         public function getGambar(){
             $this->form_validation->set_rules('x','KodeKaryawan','required|xss_clean|trim');
             if($this->form_validation->run() == TRUE){
                 $kode = $this->input->post('x');
                 $res = $this->model_utama->getGambar($kode);
-                
+
                 if($res){
                     echo $res;
                 }else{
